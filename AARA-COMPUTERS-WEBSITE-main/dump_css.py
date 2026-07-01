a76bd7cd331692f6eccb8d24ef673ed66b37e70d
AHMAD ANAS P S <ahmadanas10504@gmail.com>
2026-07-01 19:39:55 +0530

    Remove temp files

--- AARA-COMPUTERS-WEBSITE-main/dump_css.py
+++
@@ -1,8 +0,0 @@
-import re
-c = open('index.html', encoding='utf-8').read()
-css = re.search(r'<style>.*?</style>', c, re.DOTALL)
-if css:
-    css_text = css.group(0)
-    for line in css_text.split('\n'):
-        if 'course' in line:
-            print(line)
